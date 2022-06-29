9a18c5fe6c1cb207fb47fb7252f2357355d3711b
serkansyalcin <serkan.syalcin@hotmail.com>
2022-06-29 22:48:02 +0300

    General settings Update

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,7 @@ import './styles.scss';
 export default function header() {
     return (
         <header className="header">
-            <h1>Moneytolia</h1>
+            <h1>React.js Campaigns and Login App</h1>
         </header>
     )
 }
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate, Link } from "react-router-dom";
-import logo from '../../assets/img/logo.png';
+import logo from '../../assets/img/React.png';
 import './styles.scss';
 
 
